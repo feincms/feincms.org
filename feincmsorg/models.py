6a7878dc4b1fdec0f9b4a614c9209e5b536a5af1
Fabian Germann <fg@feinheit.ch>
2011-09-14 17:41:56 +0200

    new template for articlecontent for main content area

--- feincmsorg/models.py
+++ feincmsorg/models.py
@@ -11,7 +11,7 @@ from feincms.content.application.models import ApplicationContent
 from feincms.content.medialibrary.v2 import MediaFileContent
 from feincms.content.richtext.models import RichTextContent
 
-from feincms.admin.editor import ItemEditorForm
+from feincms.admin.item_editor import FeinCMSInline, ItemEditorForm
 from feincms_oembed.contents import OembedContent
 from form_designer.models import FormContent
 
@@ -40,43 +40,34 @@ Page.register_templates({
 
 
 class ArticleContentAdminForm(ItemEditorForm):
-    text = forms.CharField(widget=forms.Textarea, required=False, label=_('text'))
-
     def __init__(self, *args, **kwargs):
         super(ArticleContentAdminForm, self).__init__(*args, **kwargs)
         self.fields['text'].widget.attrs.update({'class': 'item-richtext'})
 
 
-class ArticleContent(models.Model):
-    # Choices for link target
-    DEFAULT = 1
-    BLANK = 2
-    SELF = 3
-    PARENT = 4
-    TOP = 5
-    TARGET_CHOICES = (
-        (DEFAULT, 'default'),
-        (BLANK, '_blank'),
-        (SELF, '_self'),
-        (PARENT, '_parent'),
-        (TOP, '_top'),
-    )
+class ArticleContentInline(FeinCMSInline):
+    form = ArticleContentAdminForm
+    raw_id_fields = ('mediafile',)
 
-    feincms_item_editor_form = ArticleContentAdminForm
+
+class ArticleContent(models.Model):
+    feincms_item_editor_inline = ArticleContentInline
 
     title = models.CharField(max_length=200, blank=True)
-    text = models.TextField(blank=True)
+    text = models.TextField(_('text'), blank=True)
     link = models.URLField(verify_exists=False, blank=True)
     link_text = models.CharField(max_length=50, blank=True)
-    link_target = models.IntegerField(choices=TARGET_CHOICES, default=DEFAULT, blank=True)
     mediafile = MediaFileForeignKey(MediaFile, blank=True, null=True)
 
     class Meta:
         abstract = True
         verbose_name = "Article Content"
-        
+
     def render(self, **kwargs):
-        return render_to_string("content/article/default.html", {'article': self})
+        return render_to_string([
+            "content/article/%s.html" % self.region,
+            "content/article/default.html",
+            ], {'content': self})
 
 
 Page.register_extensions('changedate', 'navigation', 'ct_tracker')
@@ -84,5 +75,4 @@ Page.create_content_type(RichTextContent, regions=('main', 'sidebar', 'moodboard
 Page.create_content_type(MediaFileContent, TYPE_CHOICES=MEDIA_TYPE_CHOICES)
 Page.create_content_type(OembedContent, DIMENSION_CHOICES=MEDIA_TYPE_CHOICES, regions=('main',))
 Page.create_content_type(FormContent)
-#Page.create_content_type(OembedContent)
 Page.create_content_type(ArticleContent)
