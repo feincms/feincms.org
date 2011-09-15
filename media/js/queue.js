b053226853a84a4b1cc176b7cfeaefe0823539c0
Matthias Kestenholz <mk@spinlock.ch>
2011-09-15 18:31:31 +0200

    Remove more unneeded cruft

--- media/js/queue.js
+++
@@ -1,6 +0,0 @@
-// A very simple script queue for inline scripts
-SQ = {
-    queue: new Array(),
-    add: function(f) { if (typeof f == 'function') { this.queue.push(f); } },
-    run: function() { while(this.queue.length > 0) { f = this.queue.pop(); f(); } }
-}
