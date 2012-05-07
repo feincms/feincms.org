76db76ca6eb957ce238590e2474ec4eb6739fcf4
Simon Bächler <sb@feinheit.ch>
2012-05-07 12:00:05 +0200

    add edit view to app_library

--- feincmsorg/app_library/views.py
+++ feincmsorg/app_library/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render, get_object_or_404
 from django.template.defaultfilters import slugify
+from feincms.content.application.models import app_reverse
 from feincmsorg.app_library.models import AppPromo, AppPromoForm, AppPromoTranslation
 
 def app_list(request):
@@ -19,7 +20,6 @@ def app_submit(request):
         form = AppPromoForm(request.POST, request.FILES)
         if form.is_valid():
             app = form.save(commit=False)
-            app.slug = slugify(app.title)
             app.author = request.user
             app.save()
             translation = AppPromoTranslation(parent=app, language_code='en')
@@ -29,4 +29,27 @@ def app_submit(request):
             return HttpResponseRedirect(app.get_absolute_url())
     else:
         form = AppPromoForm()
-    return render(request, 'app_library/submit.html', {'form': AppPromoForm })
+    action = app_reverse('app_library_submit', 'app_library.urls')
+    return render(request, 'app_library/submit.html', {'form': form, 'action': action })
+
+@login_required(login_url='/accounts/login/')
+def app_edit(request, slug):
+    app = get_object_or_404(AppPromo, slug=slug)
+    if not app.author == request.user:
+        return HttpResponseForbidden('Only the original uploader can edit an app.')
+    if request.method == 'POST':
+        form = AppPromoForm(request.POST, request.FILES, instance=app)
+        if form.is_valid():
+            app = form.save()
+            translation = AppPromoTranslation.objects.get(parent=app, language_code='en')
+            translation.short_description = form.cleaned_data['short_description']
+            translation.long_description = form.cleaned_data['long_description']
+            translation.save()
+            return HttpResponseRedirect(app.get_absolute_url())
+    else:
+        data = {'short_description': app.translation.short_description,
+            'long_description': app.translation.long_description }
+        form = AppPromoForm(initial=data, instance=app)
+
+    action = app_reverse('app_library_edit', 'app_library.urls', kwargs={'slug': app.slug})
+    return render(request, 'app_library/submit.html', {'form': form, 'action': action })
