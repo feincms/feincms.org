c9ed2cab5c96bd33487e64490f8a17f9f5340988
Simon Bächler <sb@feinheit.ch>
2012-05-16 10:44:21 +0200

    add app categories

--- feincmsorg/app_library/admin.py
+++ feincmsorg/app_library/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from feincms.translations import admin_translationinline
-from .models import AppPromo, AppPromoTranslation
+from feincms.translations import admin_translationinline, short_language_code
+from .models import AppPromo, AppPromoTranslation, Category, CategoryTranslation
+from django.utils.translation import ugettext_lazy as _
 
 
 #TODO: Add richtext class to long_description and TinyMCE to form.
@@ -11,3 +12,19 @@ class AppPromoAdmin(admin.ModelAdmin):
     inlines = [admin_translationinline(AppPromoTranslation)]
 
 admin.site.register(AppPromo, AppPromoAdmin)
+
+CategoryTranslationInline = admin_translationinline(CategoryTranslation, prepopulated_fields={
+    'slug': ('title',)})
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryTranslationInline]
+    list_display = ['__unicode__', 'entries']
+    search_fields = ['translations__title']
+
+    def entries(self, obj):
+        if 'translations' in getattr(AppPromo, '_feincms_extensions', ()):
+            return AppPromo.objects.filter(categories=obj, language=short_language_code()).count()
+        return AppPromo.objects.filter(categories=obj)
+    entries.short_description = _('Apps in category')
+
+admin.site.register(Category, CategoryAdmin)
